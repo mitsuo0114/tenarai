5517b9c8b617b29f92d155e04c818a8a3e44c110
Takahiro Ono <oono0114@gmail.com>
2020-05-07 19:30:45 +0900

    simple practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,126 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ProgramGenerator} from "./program-generator";
+import {WhiteBoardEventHandler} from "./whiteboard";
+
+
+function ProblemNum(props: any) {
+    const url = "https://chart.apis.google.com/chart?cht=tx&chl=(" + props.num + ")&chs=30"
+    return (
+        <td>
+            <img className={"problemnum"} src={url}/>
+        </td>
+    );
+}
+
+function SmallProblem(props: any) {
+    const url = "https://chart.apis.google.com/chart?cht=tx&chl=" + encodeURIComponent(props.mathtext) + "&chs=30"
+    return (
+        <td>
+            <pre>{props.question}</pre>
+            <img src={url}/>
+        </td>
+    );
+}
+
+function Answer(props: any) {
+    const url = "https://chart.apis.google.com/chart?cht=tx&chl=" + encodeURIComponent(props.answer) + "&chs=30"
+    return (
+        <td>
+            <img src={url}/>
+        </td>
+    );
+}
+
+class WhiteBoard extends React.Component {
+    myRef: any
+    handler: any
+
+    constructor(props: any) {
+        super(props)
+        this.myRef = React.createRef();
+        this.handler = new WhiteBoardEventHandler(this.myRef)
+    }
+
+    componentDidMount(){
+        this.handler.init()
+    }
+
+    render() {
+        // because of passive property issue, I cannot setup touch event here.
+        // https://github.com/facebook/react/issues/6436
+        return (
+            <td>
+                <canvas ref={this.myRef}
+                        onMouseDown={(event) => this.handler.onMouseDown(event)}
+                        onMouseMove={(event) => this.handler.onMouseMove(event)}
+                        onMouseUp={(event) => this.handler.onMouseUp(event)}
+                        className="canvas" width="400px" height="200px"></canvas>
+            </td>
+        )
+    }
+}
+
+function ControlButtons(props: any) {
+    return (
+        <td>
+            <p>
+                <button id="can">clear</button>
+            </p>
+            <p>
+                <button id="can">show answer</button>
+            </p>
+        </td>
+    )
+}
+
+function SelfCheckButtons(props: any) {
+    return (
+        <td>
+            <p>
+                <button id="can">OK</button>
+            </p>
+            <p>
+                <button id="can">NG</button>
+            </p>
+        </td>
+    )
+}
+
+function ProblemLine(props: any) {
+
+    return (
+        <tr>
+            <ProblemNum num={props.num}/>
+            <SmallProblem mathtext={props.mathtext} question={props.question}/>
+            <WhiteBoard/>
+            {/*<ControlButtons/>*/}
+            <Answer answer={props.answer}/>
+            <SelfCheckButtons/>
+        </tr>
+    )
+}
+
+
+function ProblemTable(props: any) {
+    var problems = ProgramGenerator.generate();
+    var compiledlines = problems.map((problem, index) =>
+        <ProblemLine num={index + 1} mathtext={problem.mathtext} question={problem.question} answer={problem.answer}/>
+    );
+    return (
+        <table>
+            {compiledlines}
+        </table>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProblemTable/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/whiteboard.ts
@@ -0,0 +1,122 @@
+import React from "react";
+
+class WhiteBoardEventHandler {
+    can: any
+    ct: any
+    mf: boolean = false
+    ox: number = 0
+    oy: number = 0
+    x: number = 0
+    y: number = 0
+    ref: any
+
+    constructor(ref: any) {
+        this.ref = ref
+    }
+
+    init() {
+        //初期設定
+        this.can = this.ref.current;
+        this.ct = this.can.getContext("2d");
+        this.ct.strokeStyle = "#000000";
+        this.ct.lineWidth = 2;
+        this.ct.lineJoin = "round";
+        this.ct.lineCap = "round";
+
+        this.can.addEventListener("touchstart", this.onTouchStart.bind(this), {passive: false})
+        this.can.addEventListener("touchmove", this.onTouchMove.bind(this), {passive: false})
+        this.can.addEventListener("touchend", this.onTouchEnd.bind(this), {passive: false})
+        this.clearCan();
+    }
+
+    getAbsolutePosition(elm: any) {
+        const {left, top} = elm.getBoundingClientRect();
+        const {left: bleft, top: btop} = document.body.getBoundingClientRect();
+        return {
+            left: left - bleft,
+            top: top - btop,
+        };
+    }
+
+    onTouchStart(event: any) {
+        console.log("Start onTouchStart====")
+        this.mf = true;
+        var target = this.getAbsolutePosition(event.target)
+        this.ox = event.touches[0].pageX - target.left;
+        this.oy = event.touches[0].pageY - target.top;
+        console.log("target.left" + target.left)
+        console.log("target.top" + target.top)
+        console.log("event.touches[0].pageX" + event.touches[0].pageX)
+        console.log("event.touches[0].pageY" + event.touches[0].pageY)
+        console.log("event.target.getBoundingClientRect().top" + event.target.getBoundingClientRect().top)
+        console.log("event.target.getBoundingClientRect().left" + event.target.getBoundingClientRect().left)
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+    onTouchMove(event: any) {
+        if (this.mf) {
+            var target = this.getAbsolutePosition(event.target)
+            this.x = event.touches[0].pageX - target.left;
+            this.y = event.touches[0].pageY - target.top;
+            this.drawLine();
+            this.ox = this.x;
+            this.oy = this.y;
+        }
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+    onTouchEnd(event: any) {
+        console.log("Start onTouchEnd====")
+        // console.log("event.touches[0].pageX" + event.touches[0].pageX)
+        // console.log("event.touches[0].pageY" + event.touches[0].pageY)
+        console.log("event.target.getBoundingClientRect().top" + event.target.getBoundingClientRect().top)
+        console.log("event.target.getBoundingClientRect().left" + event.target.getBoundingClientRect().left)
+        this.mf = false;
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+    onMouseDown(event: any) {
+        console.log("Start onMouseDown====")
+        this.ox = event.clientX - event.target.getBoundingClientRect().left;
+        this.oy = event.clientY - event.target.getBoundingClientRect().top;
+        this.mf = true;
+    }
+
+    onMouseMove(event: any) {
+        console.log("Start onMouseMove====")
+        if (this.mf) {
+            this.x = event.clientX - event.target.getBoundingClientRect().left;
+            this.y = event.clientY - event.target.getBoundingClientRect().top;
+            this.drawLine();
+            this.ox = this.x;
+            this.oy = this.y;
+        }
+    }
+
+    onMouseUp(event: any) {
+        console.log("Start onMouseUp====")
+        this.mf = false;
+    }
+
+    drawLine() {
+        this.ct.beginPath();
+        this.ct.moveTo(this.ox, this.oy);
+        this.ct.lineTo(this.x, this.y);
+        this.ct.stroke();
+    }
+
+    clearCan() {
+        this.ct.fillStyle = "rgb(255,255,255)";
+        this.ct.fillRect(0, 0,
+            this.can.getBoundingClientRect().width,
+            this.can.getBoundingClientRect().height);
+    }
+}
+
+export {WhiteBoardEventHandler}
